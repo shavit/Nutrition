4d65e8cdd782521371d4356caa0105a158ef66f0
Shavit Tzuriel <stmmsn@gmail.com>
2016-07-29 19:11:20 -0400

    🍠

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -17,6 +17,13 @@ type NutrientItem struct {
 	Group string `json:"group"`
 	Offset int `json:"offset"`
 }
+type Nutrient struct {
+	id int `json:"nutrient_id"`
+	Name string `json:"name"`
+	Group string `json:"group"`
+	Unit string `json:"unit"`
+	Value float64 `json:"value"`
+}
 type NutrientList struct {
 	SearchTerm string `json:"q"`
 	Sr string `json:"sr"`
@@ -27,8 +34,23 @@ type NutrientList struct {
 	Sort string `json:"sort"`
 	Items []NutrientItem `json:"item"`
 }
+type FoodReport struct {
+	Sr string `json:"sr"`
+	Type string `json:"type"`
+	Food FoodReportItem `json:"food"`
+}
+type FoodReportItem struct {
+	id string `json:"ndbno"`
+	Name string `json:"name"`
+	Sd string `json:"sd"`
+	Fg string `json:"fg"`
+	Nutrients []Nutrient `json:"nutrients"`
+}
+
+
 type NutrientReport struct {
 	List NutrientList
+	Report FoodReport `json:"report"`
 }
 
 func (c App) Index() revel.Result {
@@ -56,4 +78,26 @@ func (c App) Search() revel.Result {
 		}
 
 	return c.Render(entries)
+}
+
+func (c App) Nutrient(id string) revel.Result {
+	var url string
+	var apiKey string
+	nutrient := new(NutrientReport)
+
+	apiKey, _ = revel.Config.String("app.nutrition_api_key")
+	url = "http://api.nal.usda.gov/ndb/reports/?format=json"+
+				"&type=f"+
+				"&ndbno="+string(id)+
+				"&api_key="+apiKey
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+			defer res.Body.Close()
+			json.NewDecoder(res.Body).Decode(nutrient)
+		}
+
+	return c.Render(nutrient)
 }
